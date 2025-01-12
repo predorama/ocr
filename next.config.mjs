665c4cbe0d160e4b18d408327124020db6d5b222
Predrag Vila <predorama@gmail.com>
2025-01-12 19:15:58 +0500

    changed next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    api: {
-      bodyParser: {
-        sizeLimit: '10mb', // Increase the limit to 10MB (or any size you need)
-      },
-    },
-  };
   
-  export default nextConfig;
+};
+
+export default nextConfig;
